33b69a1ea684d66f851ecc422f7623cfb860f167
Rahul Nongmeikapam <153493589+rahulnongmeikapam@users.noreply.github.com>
2026-05-20 17:30:58 +0530

    Update import for HuggingFaceEmbeddings module

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 import datetime, re
 
